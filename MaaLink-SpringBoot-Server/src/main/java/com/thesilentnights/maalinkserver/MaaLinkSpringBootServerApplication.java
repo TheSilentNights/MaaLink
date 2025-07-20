4f7435d06c3c81939604a9c417b78a896eb0d266
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-07-20 19:02:54 +0800

    start

---
+++ MaaLink-SpringBoot-Server/src/main/java/com/thesilentnights/maalinkserver/MaaLinkSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.thesilentnights.maalinkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaLinkSpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaLinkSpringBootServerApplication.class, args);
+	}
+
+}
