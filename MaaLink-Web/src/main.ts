dfed7ceb99674f7ff142847ddf2e0473776fdfa2
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-08-01 23:47:31 +0800

    开始前端

--- MaaLink-Web/src/main.ts
+++ MaaLink-Web/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import MaaLink from "./MaaLink.vue";
+import ElementPlus from "element-plus"
 
-createApp(MaaLink).mount('#app')
+const maaLink = createApp(MaaLink)
+
+maaLink.use(ElementPlus)
+maaLink.mount("#app")
